724a1e48455c3b9e84973df2568d15eb06d6ea68
Mr. Enrico <denrico@northbergen.k12.nj.us>
2022-02-25 14:04:47 -0500

    Add solution

--- main.py
+++ main.py
@@ -8,4 +8,9 @@ def sim_combat(attackers, defenders, n):
 	Remember that in Root, two dice with values from 0-3 are rolled
 	simultaneously with the attacker claiming the higher number.
 	"""
-	pass
+	score = 0
+	for _ in range(n):
+		rolls = (random.randint(0, 3), random.randint(0, 3))
+		atk_hits, def_hits = min(attackers, max(rolls)), min(defenders, min(rolls))
+		score += min(atk_hits, defenders) - min(def_hits, attackers)
+	return score / n
